1a75ec491bad62111e02018897a43d31820fae06
akshatha <akshathapoojary1110@gmail.com>
2026-04-28 19:47:39 +0530

    updated proccess cpu data and ui os

--- fetch_cpu_data/proccess_cpu_data.py
+++ fetch_cpu_data/proccess_cpu_data.py
@@ -4,35 +4,50 @@ from cpu_data_handler import ( read_cpu_usage ,
                               read_temp)
 
 import csv
+from openpyxl import Workbook
 from datetime import datetime
 import time
 
 
 def fetch_all_cpu_data():
     
-    file_path = "/home/akshatha/Project/CPU_TEMP_HANDLER/cpu_data.csv"
+    file_path = "/home/akshatha/Project/CPU_TEMP_HANDLER/cpu_data.xlsx"
     
-    with open("file_path","w" , newline = "") as f:
-        writer = csv.writer(f)
-        writer.writerow(["current_time","cpu_usage_data" , 
-                         "memory_info" , "temprature" , "average_load"])
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "CPU Data"
+
+    # Header row
+    ws.append([
+        "current_time",
+        "cpu_usage_data",
+        "memory_info",
+        "temperature",
+        "average_load"
+        ])
         
-        while True:
+    while True:
            current_time = datetime.now().strftime("%H:%M:%S")
            cpu_usage_data = read_cpu_usage()
            memory_info =read_mem_info()
-           temprature = read_temp()
+           temperature = read_temp()
            average_load = read_load_avg()
            
            #log data immediatly
-           writer.writerow([current_time,cpu_usage_data , memory_info , 
-                            temprature ,average_load])
+           ws.append([
+            current_time,
+            cpu_usage_data,
+            memory_info,
+            temperature,
+            average_load
+        ])
            
            #save data asap
-           f.flush()
+           wb.save(file_path)
         
-           time.sleep(3)
-    print("CSV created successfully")
+           #after every one second update the data to excel sheet
+           time.sleep(1)
+    print("excel created successfully")
     
     
 fetch_all_cpu_data()         
